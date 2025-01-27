7f33321224f0b6cba063f299cc9d05920915a9a3
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-01-27 19:57:02 +0545

    initial screen completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,8 +10,7 @@ export const unstable_settings = {
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+     <Stack.Screen name='index' options={{headerShown:false}}/>
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,45 @@
 import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
-const Index = () => {
+import { LinearGradient } from 'expo-linear-gradient'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import CustomButton from '~/components/CustomButton'
+const App = () => {
   return (
-    // <View className="flex-1" >
-    //     <ImageBackground source={require("../assets/meditation-images/beach.webp")} resizeMode='cover' className="flex-1">
-    //     <Text className="flex-1 justify-center ">App is great</Text>
-    //     </ImageBackground>
-      
-    // </View>
-    <View className="flex-1 justify-center items-center">
-      <Text className='text-xl text-red-600'>Hello World</Text>
+    
+    <View className="flex-1" >
+        <ImageBackground source={require("../assets/meditation-images/beach.webp")} 
+        resizeMode='cover' 
+        className="flex-1">
+         <LinearGradient 
+          colors={["rgba(0,0,0,0.2)", "rgba(0,0,0,0.7)"]}
+          style={{ 
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          }}
+        >
+          <SafeAreaView className='flex-1 justify-between px-1'>
+            <View>
+            <Text className=' text-center text-white text-4xl'>
+            Meditation
+            </Text>
+            <Text className='text-center text-white mt-3 text-2xl'>Simplifying Text For Everyone</Text>
+            </View>
+            <View>
+              <CustomButton onPress={()=>console.log("Pressed")} title="Get Started"/>
+            </View>
+            <StatusBar style='light'/>
+          </SafeAreaView>
+         
+         </LinearGradient>
+         </ImageBackground>
     </View>
+    
     
   )
 }
 
-export default Index
+export default App
