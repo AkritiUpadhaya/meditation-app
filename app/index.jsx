1ccc4897d86d32aadc1c8039739a4f1c89bac732
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-01-27 16:19:14 +0545

    nativewind error

---
+++ app/index.jsx
@@ -0,0 +1,18 @@
+import { View, Text, ImageBackground } from 'react-native'
+import React from 'react'
+import beachImage from "../assets/meditation-images/beach.webp"
+const App = () => {
+  return (
+    // <View className="flex-1" >
+    //     <ImageBackground source={require("../assets/meditation-images/beach.webp")} resizeMode='cover' className="flex-1">
+    //     <Text className="flex-1 justify-center ">App is great</Text>
+    //     </ImageBackground>
+      
+    // </View>
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
