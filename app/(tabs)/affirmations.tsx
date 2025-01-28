73afd68f27486ce97f674a53a85bdf9a63fdb5f6
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-01-28 23:07:34 +0545

    wip-affirmations index

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import Gradient from '~/components/Gradient'
 
 const Affirmations = () => {
   return (
-    <View>
-      <Text>Affirmations</Text>
+    <View className='flex-1'>
+        <Gradient colors={["#2e1f58", "#54426b", "#a790af"]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <Text className='text-white text-2xl font-bold'>Change your belief with affirmations</Text>
+        </ScrollView> 
+        </Gradient>
+      
     </View>
   )
 }
---
+++ components/GuidedAffirmations.tsx
@@ -0,0 +1,40 @@
+import { View, Text, FlatList, Pressable, Image } from 'react-native'
+import React from 'react'
+import { GalleryPreviewData } from '~/constants/models/AffirmationGallery'
+import { Link } from 'expo-router';
+
+interface GuidedAffirmationsProps{
+    title:string,
+    previews: GalleryPreviewData[];
+}
+
+const GuidedAffirmations = ({title, previews}:GuidedAffirmationsProps) => {
+  return (
+    <View className='flex-1 my-5'>
+        <View className='mb-2 '>
+        <Text className='text-white text-xl '>{title}</Text>
+        </View>
+        <View>
+            <FlatList data={previews}
+             showsHorizontalScrollIndicator={false}
+             keyExtractor={(item)=>item.id.toString()}
+             renderItem={({item})=>(
+                <Link href={`/affirmations/$(item.id)`} asChild >
+                    <Pressable>
+                        <View className='h-36 w-32 rounded-md mr-4'>
+                            <Image 
+                            source={item.image}
+                            resizeMode='cover'
+                            className='w-full h-full'/>
+
+                        </View>
+                    </Pressable>
+                </Link>
+             )}/> 
+        </View>
+     
+    </View>
+  )
+}
+
+export default GuidedAffirmations
