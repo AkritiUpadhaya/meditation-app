f2eb340895f4fa2f0c6224e18ff257de95adf78e
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-01-29 22:40:52 +0545

    image render solved

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,8 +1,11 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import { GalleryPreviewData } from '~/constants/models/AffirmationGallery';
 import AFFIRMATION_GALLERY from '~/constants/affirmation-gallery';
+import Gradient from '~/components/Gradient';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const AffirmationPractice = () => {
     const {itemId} =useLocalSearchParams();
@@ -19,9 +22,23 @@ const AffirmationPractice = () => {
         }
     },[])
   return (
-    <View>
+    <View className='flex-1'>
         <ImageBackground source={affirmation?.image} resizeMode='cover' className='flex-1'>
-            
+            <Gradient colors={['rgba(0,0,0,0.5)', 'rgba(0,0,0,0,0.9)'] }>
+                <Pressable onPress={()=>router.back()} className='absolute top-16 left-6 z-10'>
+                <AntDesign name="leftcircle" size={50} color="white" />
+                </Pressable>
+                <ScrollView className='m-20' showsVerticalScrollIndicator={false}>
+                    <View className='h-full justify-center'>
+                        <View className='h-4/5 justify-center'>
+                        <Text>
+                        {affirmation?.text}
+                        </Text>
+                           
+                        </View>
+                    </View>
+                </ScrollView>
+            </Gradient>
         </ImageBackground>
     </View>
   )
